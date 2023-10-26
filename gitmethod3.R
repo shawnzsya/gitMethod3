3202ef82de50bf1107e109891de4b79c197e7a16
shawnzsya <99060745+shawnzsya@users.noreply.github.com>
2023-10-26 17:04:21 +0800

    Update gitmethod3.R

--- gitmethod3.R
+++ gitmethod3.R
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
+
 library(usethis)
 edit_git_config()
 use_github()
-=======
-hello world
 
-ashaids
->>>>>>> e14b6a2150499bda37504a916dc6d2d17f11c8c3
+library(a)
+library(b)
+library(c)
+
+
